8b9501fada47861141aa356f5d8720837a6c7629
1Ahmedzedan <a7mdzedan199@gmail.com>
2024-08-20 13:01:53 +0300

    Add solutions sheet for CPU-Standard-L1-C106

---
+++ Level 1/CPU - Standard - L1 - C106/Practice G4/Week 01/J.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+int main(){
+    // int x ; 
+    // cin >> x ; 
+    // cout << (x / 5) + (x % 5 != 0) ;  
+    // true (1) , false (0)
+    double x ; 
+    cin >> x ; 
+    cout << ceil(x/5) ; 
+    return 0 ; 
+}
